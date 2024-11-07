e8a040b554d980f409fa015d045bc23dbb707199
DonPadla <vovavoroncov@yandex.ru>
2024-11-07 20:47:41 +0300

    adjustment

--- src/processing.py
+++ src/processing.py
@@ -2,9 +2,9 @@ def filter_by_state(list_of_dictionaries: list, state: str = 'EXECUTED') -> list
     """ Принимает список словарей и опционально значение для ключа state, возвращает новый список словарей,
     содержащий только те словари, у которых ключ state соответствует указанному значению """
     filtered_list = []
-    for dictionari in list_of_dictionaries:
-        if dictionari.get('state') == state:
-            filtered_list.append(dictionari)
+    for dictionary in list_of_dictionaries:
+        if dictionary.get('state') == state:
+            filtered_list.append(dictionary)
     return filtered_list
 
 
